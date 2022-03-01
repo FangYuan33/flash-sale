fb8872bb31d7d995cc9045cb7a87566ddaeea1a9
wangyilong13 <wangyilong13@jd.com>
2022-03-01 20:34:27 +0800

    基础功能：规范了初始化SQL 去掉了不用的代码

--- flash-sale-domain/src/main/java/com/actionworks/flashsale/domain/model/enums/FlashActivityStatus.java
+++ flash-sale-domain/src/main/java/com/actionworks/flashsale/domain/model/enums/FlashActivityStatus.java
@@ -23,14 +23,6 @@ public enum FlashActivityStatus {
         this.code = code;
     }
 
-    public static boolean isOffline(Integer status) {
-        return OFFLINE.getCode().equals(status);
-    }
-
-    public static boolean isOnline(Integer status) {
-        return ONLINE.getCode().equals(status);
-    }
-
     public Integer getCode() {
         return code;
     }
