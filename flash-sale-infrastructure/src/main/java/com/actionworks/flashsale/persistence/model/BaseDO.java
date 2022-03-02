549a33d1cbdca651d39164e42dcad645fd8773bf
wangyilong13 <wangyilong13@jd.com>
2022-03-02 16:07:05 +0800

    框架搭建：引入mybatis-plus 必要补充，时间赋值处理器

--- flash-sale-infrastructure/src/main/java/com/actionworks/flashsale/persistence/model/BaseDO.java
+++ flash-sale-infrastructure/src/main/java/com/actionworks/flashsale/persistence/model/BaseDO.java
@@ -1,5 +1,9 @@
 package com.actionworks.flashsale.persistence.model;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -7,9 +11,12 @@ import java.time.LocalDateTime;
 @Data
 public class BaseDO {
 
+    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
+    @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createTime;
 
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime modifiedTime;
 }
