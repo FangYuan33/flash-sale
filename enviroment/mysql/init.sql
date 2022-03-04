eaf4fd632c25f1b545a35493297be97233ae05b3
fangyuan <7429032+fangyuan33@user.noreply.gitee.com>
2022-03-04 20:13:47 +0800

    基础功能：整理了一下秒杀订单建表SQL

--- enviroment/mysql/init.sql
+++ enviroment/mysql/init.sql
@@ -32,33 +32,19 @@ CREATE TABLE IF NOT EXISTS flash_sale.`flash_item` (
     PRIMARY KEY (`id`)
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = '秒杀品';
 
+-- 初始化秒杀订单表
 CREATE TABLE IF NOT EXISTS flash_sale.`flash_order` (
-    `id`            bigint(20)  NOT NULL AUTO_INCREMENT
-    COMMENT '主键',
-    `item_id`       bigint(20)  NOT NULL
-    COMMENT '秒杀品ID',
-    `activity_id`   bigint(20)  NOT NULL
-    COMMENT '秒杀活动ID',
-    `item_title`    varchar(50) NOT NULL
-    COMMENT '秒杀品名称标题',
-    `flash_price`   bigint(20)  NOT NULL
-    COMMENT '秒杀价',
-    `quantity`      int(11)     NOT NULL
-    COMMENT '数量',
-    `total_amount`  bigint(20)  NOT NULL
-    COMMENT '总价格',
-    `status`        int(11)     NOT NULL DEFAULT '0'
-    COMMENT '订单状态',
-    `user_id`       bigint(20)  NOT NULL
-    COMMENT '用户ID',
-    `modified_time` datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP
-    COMMENT '更新时间',
-    `create_time`   datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP
-    COMMENT '创建时间',
+    `id`            bigint(20)  NOT NULL AUTO_INCREMENT COMMENT '秒杀订单ID',
+    `item_id`       bigint(20)  NOT NULL COMMENT '秒杀品ID',
+    `activity_id`   bigint(20)  NOT NULL COMMENT '秒杀活动ID',
+    `user_id`       bigint(20)  NOT NULL COMMENT '用户ID',
+    `item_title`    varchar(50) NOT NULL COMMENT '秒杀品名称',
+    `flash_price`   bigint(20)  NOT NULL COMMENT '秒杀价',
+    `quantity`      int(11)     NOT NULL COMMENT '数量',
+    `total_amount`  bigint(20)  NOT NULL COMMENT '总价格',
+    `status`        int(11)     NOT NULL DEFAULT '0' COMMENT '订单状态',
+    `modified_time` datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+    `create_time`   datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     PRIMARY KEY (`id`),
-    UNIQUE KEY `flash_order_id_uk` (`id`),
     KEY `flash_order_user_id_idx` (`user_id`)
-    )
-    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COMMENT = '秒杀订单表';
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = '秒杀订单表';
