01483a4d028a3ea116abc33db66b3b36adb793fa
wyl <374072213@qq.com>
2022-03-12 12:27:12 +0800

    分布式缓存：引入redisson依赖，加入redisson配置文件

---
+++ flash-sale-infrastructure/src/main/java/com/actionworks/flashsale/cache/redis/RedissonConfig.java
@@ -0,0 +1,25 @@
+package com.actionworks.flashsale.cache.redis;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+    @Value("${spring.redis.address}")
+    private String address;
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress(address).setPassword(password);
+
+        return Redisson.create(config);
+    }
+}
