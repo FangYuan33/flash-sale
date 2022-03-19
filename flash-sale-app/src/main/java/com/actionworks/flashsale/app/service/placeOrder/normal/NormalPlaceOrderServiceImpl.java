98715ef7f93c1b539688bd52f00fdd73fa9f248d
fangyuan <374072213@qq.com>
2022-03-19 17:08:21 +0800

    库存缓存：重写了秒杀下单的方法

--- flash-sale-app/src/main/java/com/actionworks/flashsale/app/service/placeOrder/normal/NormalPlaceOrderServiceImpl.java
+++ flash-sale-app/src/main/java/com/actionworks/flashsale/app/service/placeOrder/normal/NormalPlaceOrderServiceImpl.java
@@ -8,6 +8,7 @@ import com.actionworks.flashsale.app.model.result.AppResult;
 import com.actionworks.flashsale.app.service.activity.FlashActivityAppService;
 import com.actionworks.flashsale.app.service.item.FlashItemAppService;
 import com.actionworks.flashsale.app.service.placeOrder.PlaceOrderService;
+import com.actionworks.flashsale.cache.ItemStockCacheService;
 import com.actionworks.flashsale.domain.model.entity.FlashOrder;
 import com.actionworks.flashsale.domain.model.stock.StockDeduction;
 import com.actionworks.flashsale.domain.service.FlashOrderDomainService;
@@ -15,7 +16,6 @@ import com.actionworks.flashsale.domain.service.StockDeductionDomainService;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
@@ -34,9 +34,10 @@ public class NormalPlaceOrderServiceImpl implements PlaceOrderService {
     private StockDeductionDomainService stockDeductionDomainService;
     @Resource
     private FlashOrderDomainService flashOrderDomainService;
+    @Resource
+    private ItemStockCacheService itemStockCacheService;
 
     @Override
-    @Transactional
     public <T> AppResult<T> doPlaceOrder(Long userId, FlashPlaceOrderCommand command) {
         log.info("doPlaceOrder|开始下单|{}, {}", userId, JSON.toJSONString(command));
 
@@ -44,53 +45,90 @@ public class NormalPlaceOrderServiceImpl implements PlaceOrderService {
         checkPlaceOrderCondition(command.getActivityId(), command.getItemId());
 
         // 扣减库存
-        decreaseItemStock(command.getItemId(), command.getQuantity());
+        boolean decreaseSuccess = decreaseItemStock(command.getItemId(), command.getQuantity());
 
-        // 初始化秒杀订单信息
-        FlashOrder flashOrder = initialFlashOrderInfo(userId, command);
+        // 库存扣减成功，下订单
+        if (decreaseSuccess) {
+            createFlashOrder(userId, command);
 
-        // 秒杀订单入库
-        flashOrderDomainService.doPlaceOrder(flashOrder);
-        log.info("doPlaceOrder|下单成功");
-
-        return AppResult.success();
+            return AppResult.success();
+        } else {
+            throw new BizException(DO_PLACE_ORDER);
+        }
     }
 
     /**
      * 校验秒杀活动、秒杀时间条件
      *
      * @param activityId 秒杀活动id
-     * @param itemId 秒杀商品id
+     * @param itemId     秒杀商品id
      */
     private void checkPlaceOrderCondition(Long activityId, Long itemId) {
         boolean activityAllow = flashActivityAppService.isAllowPlaceOrderOrNot(activityId);
         boolean itemAllow = flashItemAppService.isAllowPlaceOrderOrNot(itemId);
 
         if (!activityAllow || !itemAllow) {
-             throw new BizException(DO_PLACE_ORDER);
+            throw new BizException(DO_PLACE_ORDER);
         }
     }
 
     /**
      * 扣减库存
      *
-     * @param itemId 秒杀商品ID
+     * @param itemId   秒杀商品ID
      * @param quantity 扣减数量
      */
-    private void decreaseItemStock(Long itemId, Integer quantity) {
+    private boolean decreaseItemStock(Long itemId, Integer quantity) {
+        boolean cacheSuccess = false;
+        boolean dbSuccess = false;
         StockDeduction stockDeduction = new StockDeduction().setItemId(itemId).setQuantity(quantity);
-        boolean success = stockDeductionDomainService.decreaseItemStock(stockDeduction);
 
-        if (!success) {
-            log.error("doPlaceOrder|扣减库存失败|{}", JSON.toJSONString(stockDeduction));
-            throw new BizException(DO_PLACE_ORDER);
+        try {
+            // 预扣减缓存库存
+            cacheSuccess = itemStockCacheService.decreaseItemStock(itemId, quantity);
+            if (!cacheSuccess) {
+                return false;
+            }
+
+            // 扣减数据库缓存
+            dbSuccess = stockDeductionDomainService.decreaseItemStock(stockDeduction);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            // 缓存扣减成功但是出现了异常，需要将缓存库存恢复
+            if (cacheSuccess) {
+                itemStockCacheService.increaseItemStock(itemId, quantity);
+            }
+        }
+
+        return cacheSuccess && dbSuccess;
+    }
+
+    /**
+     * 下单
+     */
+    private void createFlashOrder(Long userId, FlashPlaceOrderCommand command) {
+        try {
+            // 初始化秒杀订单信息
+            FlashOrder flashOrder = initialFlashOrderInfo(userId, command);
+
+            // 秒杀订单入库
+            flashOrderDomainService.doPlaceOrder(flashOrder);
+            log.info("doPlaceOrder|下单成功");
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            // 下单失败恢复缓存和数据库库存
+            itemStockCacheService.increaseItemStock(command.getItemId(), command.getQuantity());
+
+            StockDeduction stockDeduction =
+                    new StockDeduction().setItemId(command.getItemId()).setQuantity(command.getQuantity());
+            stockDeductionDomainService.increaseItemStock(stockDeduction);
         }
     }
 
     /**
      * 初始化秒杀订单信息
      *
-     * @param userId 用户ID
+     * @param userId  用户ID
      * @param command 包含订单中必要信息
      */
     private FlashOrder initialFlashOrderInfo(Long userId, FlashPlaceOrderCommand command) {
