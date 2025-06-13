7fe11a9859cb697ed9597333dd9ad887134eef6f
FangYuan <374072213@qq.com>
2025-06-13 20:16:00 +0800

    DDD: 完善内容

---
+++ flash-sale-application/src/main/java/com/actionworks/flashsale/application/ability/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 在一些 DDD 的项目中会在该包下定义一个个能力点，在应用层做服务的编排
+ */
+package com.actionworks.flashsale.application.ability;
---
+++ flash-sale-application/src/main/java/com/actionworks/flashsale/application/adapter/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 主要放一些与领域服务无关的外部接口调用适配器
+ */
+package com.actionworks.flashsale.application.adapter;
